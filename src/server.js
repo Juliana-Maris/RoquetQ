a6bf9929b566baa7868712a23342469cabd1103f
José Moreira <canasvieiras@hotmail.com.br>
2021-06-25 19:31:47 -0300

    aula 04

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const route = require('./route');
 const path = require('path');
-
 const server = express();
 
 server.set('view engine', 'ejs');
@@ -9,6 +8,8 @@ server.set('view engine', 'ejs');
 server.use(express.static("public"));
 
 server.set('views', path.join(__dirname, 'views'));
+
+server.use(express.urlencoded({ extend: true }));
 
 server.use(route);
 
